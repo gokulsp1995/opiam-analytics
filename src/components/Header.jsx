367dbf3033ffe24cb5a558816bd587c49e0ed9e5
gokulsp1995 <gokulsp2020@outlook.com>
2024-02-28 11:45:30 +0530

    Mobie view header aligned

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,12 +9,12 @@ const Header = () => {
     return (
         <header className="header">
             <div className="logo-container">
-                <img src={logo} alt="Company Logo" className="logo" />
+                {/* <img src={logo} alt="Company Logo" className="logo" /> */}
                 
             </div>
             <div className="name-image">
                 <div className="content-container">
-                    <h1 className="company-name">OPIAM <br/> ANALYTICS</h1>
+                    {/* <h1 className="company-name">OPIAM <br/> ANALYTICS</h1> */}
                     <h2 className="main-heading">Data Analytics <br/> and AI for the Construction <br/> Industry</h2>
                     <h3 className="sub-heading">Make Data-Driven Decisions</h3>
                     <p className="contact-info">SCHEDULE A CALL</p>
