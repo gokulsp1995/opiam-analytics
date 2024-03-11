8a5406fac58e29cf206f94a2bc86e3f204139bb0
gokulsp1995 <gokulsp2020@outlook.com>
2024-03-11 16:22:02 +0530

    Animation made single elements

--- src/components/Integration.jsx
+++ src/components/Integration.jsx
@@ -26,10 +26,11 @@ useEffect(() => {
 }, []);
 
     return (
-        <div ref={headerRef} className={`integration ${isVisible ? 'animate' : ''}`}>
+        <div ref={headerRef} className="integration">
+         {/* {`integration ${isVisible ? 'animate' : ''}`} */}
             <h1 className="">Seamless <br/> Integration</h1>
-            <img src={image1} alt="image1" className="first slide-in-from-left"/>
-            <img src={image2} alt="image2" className="second slide-in-from-right"/>
+            <img src={image1} alt="image1" className={`first ${isVisible ? 'slide-in-from-left' : ''}`}/>
+            <img src={image2} alt="image2" className= {`second ${isVisible ? 'slide-in-from-right' : ''}`}/>
             <p className="">Our solutions will work with your <br/> existing systems to create more value</p>
         </div>
     )
--- src/components/Package.jsx
+++ src/components/Package.jsx
@@ -29,14 +29,14 @@ const Package = () => {
                 <h3>Choose from our affordable and customizable packages.</h3>
             </div>
             <div className="three-para">
-                <div className={`element ${isVisible ? 'rise-in' : ''}`}>
+                <div className={`element1 ${isVisible ? 'rise-in' : ''}`}>
                     <h1>Foundation</h1>
                     <ul>
                         <li>Basic package</li>
                         <li>Pre-Built Analytical Dashboards</li>
                     </ul>
                 </div>
-                <div className={`element ${isVisible ? 'rise-in' : ''}`}>
+                <div className={`element2 ${isVisible ? 'rise-in' : ''}`}>
                     <h1>Solution</h1>
                     <ul>
                         <li>Targeted package</li>
@@ -44,7 +44,7 @@ const Package = () => {
                         <li>Functional AI Agent</li>
                     </ul>
                 </div>
-                <div className={`element ${isVisible ? 'rise-in' : ''}`}>
+                <div className={`element3 ${isVisible ? 'rise-in' : ''}`}>
                     <h1>Digit-All</h1>
                     <ul>
                         <li>Full package</li>
