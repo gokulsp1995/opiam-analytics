eb8f5ee6df916c512148b0b48a03920fb30cc5ea
gokulsp1995 <gokulsp2020@outlook.com>
2024-03-11 16:41:15 +0530

    Body Animation para resetted

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -31,22 +31,24 @@ const Body = () => {
     return (
         <div 
         ref={headerRef} 
-        className={`body ${isVisible ? 'animate' : ''}`} 
+        className="body"
         >
-            <h1 className="slide-in-from-left">Digital First for the Construction Industry</h1>
+            {/* {`${isVisible ? 'slide-in-from-left' : ''}`}  */}
+            {/* {`${isVisible ? 'slide-in-from-right' : ''}`}  */}
+            <h1 className={`${isVisible ? 'slide-in-from-left' : ''}`}>Digital First for the Construction Industry</h1>
 
-            <h3 className="slide-in-from-right animate-on-scroll">Data Analytics</h3>
-            <container><p className="popup-text">Dive deep into the heart of your project's data to uncover actionable insights and foresights. Our interconnected <b>series of performance dashboards</b> provide real-time visibility critical project metrics, enabling you identify gaps, predict potential delays, optimize costs effectively.</p>
+            <h3 className={`${isVisible ? 'slide-in-from-right' : ''}`}>Data Analytics</h3>
+            <container><p className={`popup-text ${isVisible ? 'slide-in-from-left' : ''}`}>Dive deep into the heart of your project's data to uncover actionable insights and foresights. Our interconnected <b>series of performance dashboards</b> provide real-time visibility critical project metrics, enabling you identify gaps, predict potential delays, optimize costs effectively.</p>
             </container>
 
-            <h3 className="slide-in-from-left animate-on-scroll">Generative AI</h3>
+            <h3 className={`${isVisible ? 'slide-in-from-right' : ''}`}>Generative AI</h3>
             <container>
-                <p className="popup-text animate-on-scroll">From enhancing project scheduling and estimation to providing advanced analytics real-time problem-solving, our Generative AI tools are designed adapt learn from your organization's unique execution patterns, ensuring continuous improvement innovation.</p>
+                <p className={`popup-text ${isVisible ? 'slide-in-from-left' : ''}`}>From enhancing project scheduling and estimation to providing advanced analytics real-time problem-solving, our Generative AI tools are designed adapt learn from your organization's unique execution patterns, ensuring continuous improvement innovation.</p>
             </container>
 
-            <h3 className="slide-in-from-right animate-on-scroll">AI Process Automation</h3>
+            <h3 className={` ${isVisible ? 'slide-in-from-right' : ''}`}>AI Process Automation</h3>
             <container>
-                <p className="popup-text animate-on-scroll">Our functional AI assistant and automated procurement system transform the way you manage projects by automating routine tasks, optimizing resource allocation, ensuring timely material procurement. </p>
+                <p className={`popup-text ${isVisible ? 'slide-in-from-left' : ''}`}>Our functional AI assistant and automated procurement system transform the way you manage projects by automating routine tasks, optimizing resource allocation, ensuring timely material procurement. </p>
             </container>
         </div>
     )
