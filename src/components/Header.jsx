4b9727c28756338e74e4a8521400965aed083623
gokulsp1995 <gokulsp2020@outlook.com>
2024-03-13 14:18:50 +0530

    Smaller screen size styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,19 +31,18 @@ const Header = () => {
 
     return (
         <header ref={headerRef} className="header">
-        {/* {`header ${isVisible ? 'animate' : ''}`} */}
+            {/* {`header ${isVisible ? 'animate' : ''}`} */}
             <div className="logo-container">
-                    <img src={logo} alt="Company Logo" className="logo" />
-                    <div class="navbar-nav ms-auto p-4 p-lg-0">
-                        <a href="index.html" class="nav-item nav-link active">Home</a>
-                        <a href="about.html" class="nav-item nav-link">About</a>
-                        <a href="service.html" class="nav-item nav-link">Service</a>
-                        <a href="contact.html" class="nav-item nav-link">Contact</a>
-                    </div>
+                <img src={logo} alt="Company Logo" className="logo" />
+                <div class="navbar-nav ms-auto p-4 p-lg-0">
+                    <a href="index.html" class="nav-item nav-link active">Home</a>
+                    <a href="about.html" class="nav-item nav-link">About</a>
+                    <a href="service.html" class="nav-item nav-link">Service</a>
+                    <a href="contact.html" class="nav-item nav-link">Contact</a>
+                </div>
             </div>
             <div className="name-image">
                 <div className="content-container">
-
                     <h2 className="main-heading">Data Analytics and AI <br /> for the Construction <br /> Industry</h2>
                     <h3 className="sub-heading">Make Data-Driven <br /> Decisions</h3>
                     <p className="contact-info">SCHEDULE A CALL</p>
